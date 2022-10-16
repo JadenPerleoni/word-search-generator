1b00f4b2168791de928032e14ee7ec551d47b134
JadenPerleoni <jadenperleoni@gmail.com>
2022-10-16 08:03:16 -0700

    adding logic

--- src/Board.js
+++ src/Board.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Letter from "./Letter";
 import "./board.css";
-import "./data/words_dictionary.json";
 var randomWords = require("random-words");
 
 function Board() {
@@ -53,37 +52,72 @@ function Board() {
 
   const wordsArr = randomWords({ exactly: NUM_WORDS, maxLength: 5 });
 
+  const checkForOverWrite = (yNum) => {
+    for (let i = 0; i < NUM_ROWS; i++) {
+      if (letters[yNum][i] !== "") {
+        return true;
+      }
+    }
+  };
+
+  // Places the letters of the random words at random locations on the board.
   while (k < NUM_WORDS) {
     let randomX = Math.floor(Math.random() * NUM_ROWS);
     let randomY = Math.floor(Math.random() * NUM_COLS);
 
+    if (checkForOverWrite(randomY)) {
+      console.log("working");
+      continue;
+    }
     if (randomX >= NUM_ROWS / 2) {
       // Writing word backwards
       for (let i = 0; i <= wordsArr[k].length; i++) {
-        letters[randomX][randomY] = wordsArr[k].charAt(i);
+        letters[randomY][randomX] = wordsArr[k].charAt(i).toUpperCase();
         randomX--;
       }
     } else {
-      // Write logic for forwards here
+      // Write word forwards
+      for (let i = 0; i <= wordsArr[k].length; i++) {
+        letters[randomY][randomX] = wordsArr[k].charAt(i).toUpperCase();
+        randomX++;
+      }
     }
-
-
-
     k++;
   }
-  console.log(wordsArr);
-  console.log(letters);
-  /*
-    Logic:
-    while WORDS PLACED < NUM_WORDS {
-        random
-    }
-  */
+  // End placement**********
 
+  // Fill the rest of the board with random letters"
+
+  for (let x = 0; x < NUM_ROWS; x++) {
+    for (let y = 0; y < NUM_COLS; y++) {
+      let rnd = Math.floor(Math.random() * alphabetArr.length);
+
+      if (letters[x][y] === "") {
+        letters[x][y] = alphabetArr[rnd];
+      }
+    }
+  }
+
+  
+  // letters.map((items, index) => {
+  //   items.map((subItems, sIndex) => {
+
+  console.log(letters);
   return (
     <div className="board">
       <div className="rectangle">
-        <Letter letter={wordsArr[1]}></Letter>
+        {/* <div className = "grid-container">
+          {letters.map((items, index) => {
+            return (
+              <div key = {index} className = "grid-item">
+                {items.map((subItems, sIndex) => {
+                  return <Letter key = {sIndex} letter = {subItems}></Letter>
+                })}
+              </div>
+            );
+          })}
+        </div> */}
+        <Letter letter = {letters}></Letter>
       </div>
     </div>
   );
