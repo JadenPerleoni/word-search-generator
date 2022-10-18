4d594d60dadc003075645bce7551a3a97c44b03d
JadenPerleoni <jadenperleoni@gmail.com>
2022-10-17 22:16:19 -0700

    sheesh

--- src/Board.js
+++ src/Board.js
@@ -7,7 +7,7 @@ function Board() {
   const NUM_WORDS = 5;
   const NUM_ROWS = 10;
   const NUM_COLS = 10;
-  const NUM_DIRECTIONS = 4;
+  const NUM_DIRECTIONS = 2;
   let k = 0;
 
   const alphabetArr = [
@@ -53,48 +53,79 @@ function Board() {
 
   const wordsArr = randomWords({ exactly: NUM_WORDS, maxLength: 5 });
 
-
   // Check for vertical ow's.
-  const checkForOverWrite = (yNum,xNum,direction) => {
-    
-    for (let i = 0; i < NUM_ROWS; i++) {
-      if (letters[yNum][i] !== "") {
-        return true;
-      }
+  const checkForOverWrite = (yNum, xNum, direction) => {
+    debugger;
+    switch (direction) {
+      case 0:
+        for (let i = 0; i < NUM_ROWS; i++) {
+          if (letters[yNum][i] !== "") {
+            return true;
+          }
+        }
+        break;
+      case 1:
+        for (let i = 0; i < NUM_COLS; i++) {
+          if (letters[i][yNum] !== "") {
+            return true;
+          }
+        }
+        break;
     }
   };
 
-  const checkForError = (yNum,xNum,direction) => {
-
-  }
-
   // Places the letters of the random words at random locations on the board.
   while (k < NUM_WORDS) {
+    debugger;
     // TODO: Write words vertically.
     let randomX = Math.floor(Math.random() * NUM_ROWS);
     let randomY = Math.floor(Math.random() * NUM_COLS);
 
-    // Picks a number between 0 and 3. 
+    // Picks a number between 0 and 3.
     let randomDir = Math.floor(Math.random() * NUM_DIRECTIONS);
 
     // Checks if the random number picked a value that will cause a word to
     // be overwritten. If it will be, it will continue.
-    if (checkForOverWrite(randomY)) {
+    if (checkForOverWrite(randomY, randomX, randomDir)) {
       continue;
     }
-    if (randomX >= NUM_ROWS / 2) {
-      // Writing word backwards
-      for (let i = 0; i <= wordsArr[k].length; i++) {
-        letters[randomY][randomX] = wordsArr[k].charAt(i).toUpperCase();
-        randomX--;
-      }
-    } else {
-      // Write word forwards
-      for (let i = 0; i <= wordsArr[k].length; i++) {
-        letters[randomY][randomX] = wordsArr[k].charAt(i).toUpperCase();
-        randomX++;
-      }
+
+    // Switch statement that picks the direction based on randomDir
+    // 0 = horizontal, 1 = vertical.
+
+    switch (randomDir) {
+      case 0:
+        if (randomX >= NUM_ROWS / 2) {
+          // Writing word backwards
+          for (let i = 0; i <= wordsArr[k].length; i++) {
+            letters[randomY][randomX] = wordsArr[k].charAt(i).toUpperCase();
+            randomX--;
+          }
+        } else {
+          // Write word forwards
+          for (let i = 0; i <= wordsArr[k].length; i++) {
+            letters[randomY][randomX] = wordsArr[k].charAt(i).toUpperCase();
+            randomX++;
+          }
+        }
+        break;
+      case 1:
+        // Write word upside down
+        if (randomY >= NUM_COLS / 2) {
+          for (let i = 0; i <= wordsArr[k].length; i++) {
+            letters[randomY][randomX] = wordsArr[k].charAt(i).toUpperCase();
+            randomY--;
+          }
+        } else {
+          // Write word downwards
+          for (let i = 0; i <= wordsArr[k].length; i++) {
+            letters[randomY][randomX] = wordsArr[k].charAt(i).toUpperCase();
+            randomY++;
+          }
+        }
+        break;
     }
+
     k++;
   }
 
@@ -121,7 +152,7 @@ function Board() {
   const totalWords = wordsArr.map((item, index) => <ul key={index}>{item}</ul>);
 
   console.log(letters);
-  console.log(letters[3][2])
+  console.log(letters[3][2]);
   return (
     <div>
       <h2>Click the letters!</h2>
